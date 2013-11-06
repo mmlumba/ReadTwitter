ebba43f54583d44a6b8cc48c571116da8c3147b0
M Lumba <mmlumba@gmail.com>
2013-11-05 23:12:38 -0500

    change timeline stuff

--- example/app_tokens.php
+++ example/app_tokens.php
@@ -3,9 +3,9 @@
 // OAuth tokens for single-user programming
 // After you create an app you MUST copy 
 // the tokens and paste them into the following lines
-$consumer_key = 'CONSUMER KEY';
-$consumer_secret = 'CONSUMER SECRET';
-$user_token = 'USER TOKEN';
-$user_secret = 'USER SECRET';
+$consumer_key = 'hJ75vlDtueJeRauUXu5ow';
+$consumer_secret = '0lh6Lejgp8C9h2ZG2TvsAn2YkiRVESMIMHFefI3sE';
+$user_token = '14255934-et8BZO1mIvU0IrCfFUMX3dOAe5POOvoAyMpOlXEZg';
+$user_secret = 'omsato3lgDz2zMMD2zHTiqkSwyPw3Llp04jwX26wSr4JH';
 
 ?>
