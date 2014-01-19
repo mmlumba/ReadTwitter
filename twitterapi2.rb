3c1c4d588eedf494fe3bcbcea69ec8d8b3bf5c04
M Lumba <mmlumba@gmail.com>
2014-01-19 17:39:58 -0500

    Changed implementation to use stream instead of REST API

--- twitterapi2.rb
+++ twitterapi2.rb
@@ -15,36 +15,69 @@ Choice.options do
 	end
 end
 
-keyWord = Array.new(option)
-keyWord.each {|x| puts x}
+#keyWord = Array.new(option)
+#keyWord.each {|x| puts x}
 
-def read_twitter(userName,word)
-	client = Twitter::REST::Client.new do |config|
+def read_twitter(word)
+	client = Twitter::Streaming::Client.new do |config|
 			config.consumer_key        = "hJ75vlDtueJeRauUXu5ow"
 			config.consumer_secret     = "0lh6Lejgp8C9h2ZG2TvsAn2YkiRVESMIMHFefI3sE"
 			config.access_token        = "14255934-et8BZO1mIvU0IrCfFUMX3dOAe5POOvoAyMpOlXEZg"
 			config.access_token_secret = "omsato3lgDz2zMMD2zHTiqkSwyPw3Llp04jwX26wSr4JH"
 	end
-	#read = client.user_timeline(userName,{:count => 200})
-	read = client.search("to: #{word * ' '}", :count => 200).collect do |tweet|
-  		"#{tweet.text}"
-	end
-	currentTime = (Time.new).strftime("%Y_%m_%d");
-	fname="#{currentTime}.txt"
+
+    topics = ['paramore']
+    
+    currentTime = (Time.new).strftime("%Y_%m_%d");
+	startTime = Time.now
+	fname="#{word.join("_")}_#{currentTime}.txt"
 	somefile=File.open(fname,"w")
 
+
+    begin
+	    
+		#puts "made it here"
+
+	    count = 0
+		    client.filter(:track => Choice.choices[:word].join(",")) do | object |
+
+		    	somefile.puts object.text if object.is_a?(Twitter::Tweet)
+		    	count +=1
+		    	if(count == 100)
+		    		abort
+		    	end
+		    end
+
+	    somefile.close
+
+	rescue Exception => e
+	    somefile.close
+	    puts e
+    end
+
+
+    #client.filter do |object|
+    #	puts object.text if object.is_a?(Twitter::Tweet)
+    #end
+	#read = client.user_timeline(userName,{:count => 200})
+	#read = client.search("to: #{word * ' '}", :count => 200).collect do |tweet|
+  #		"#{tweet.text}"
+	#end
+	
 	#read.each{ |tweet| somefile.puts(tweet.text) }
-	somefile.puts(read)
-	somefile.close
+	#somefile.puts(read)
+	#somefile.close
 end;
 
 #:result_type => "recent"
 
 
-begin
-	read_twitter Choice.choices.userName,Choice.choices.word
-rescue => e_username
-	puts(e_username)
-	puts("Username: '#{Choice.choices.userName}' does not appear to have a Twitter profile...")
-rescue => e_keyword
-end
+#begin
+#	read_twitter Choice.choices.userName,Choice.choices.word
+#rescue => e_username
+#	puts(e_username)
+#	puts("Username: '#{Choice.choices.userName}' does not appear to have a Twitter profile...")
+#rescue => e_keyword
+#end
+
+read_twitter(Choice.choices[:word])
